b569324b1ee0c66f8a246512fd0755b55a5e605f
WGierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-01-17 02:32:29 +0100

    parse new data after hard reset

--- server/updater.sh
+++ server/updater.sh
@@ -1,9 +1,9 @@
 while true
 do
 	date +"%T"
-	python server/json_parser.py
 	git fetch --all
 	git reset --hard origin/master
+	python server/json_parser.py
 	CHANGES=$(git status | grep modified: | awk '{split($0,a,"/"); print a[2]}' | sed 's/\.json//' | tr '\n' ' ')
 	git add --all
 	git commit -m "Update: $CHANGES"
